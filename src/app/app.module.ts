f486dc3e3d23bcbfa338d863b6b7906b568a4dd4
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2019-07-30 14:10:21 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DepartmentDetailComponent } from './department-detail/department-detail.component';
-import { DepartmentOverviewComponent } from './department-detail/department-overview/department-overview.component';
-import { DepartmentContactComponent } from './department-detail/department-contact/department-contact.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
